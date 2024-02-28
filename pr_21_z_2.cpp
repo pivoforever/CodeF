0eb981595805322fc4927f04597c735a1c9e4741
Vit <pivonever@gmail.com>
2024-02-28 10:45:09 +0500

    Финал. Проверял в вижаке

--- pr_21_z_2.cpp
+++ pr_21_z_2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cstdlib>
-#include <clocale>
+#include <Windows.h>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -11,76 +12,104 @@ struct product
     int quantity;
 };
 
-void acceptGoods(vector <product>);
-void shipGoods(vector <product>);
-void info(vector <product>);
-int isGoodOnStock(vector <product>, string);
+void acceptGoods(vector<product>&);
+void shipGoods(vector<product>&);
+void info(vector<product>&);
+int isGoodOnStock(vector<product>&, string);
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-    setlocale(LC_ALL, "Russian");
-    vector <product> stock={{"консервированный горошек", 10}};
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    vector<product> stock = { {"Консервированный горошек", 10} };
     int n;
-    cout<<"Веберите, что хотите сделать:"<<endl;
-    cout<<"1 - принять товар на склад:"<<endl;
-    cout<<"2 - отгрузить товар со склада:"<<endl;
-    cout<<"3 - вевести иформацию о товаре на складе:"<<endl;
-    cin>>n;
-    switch (n)
+    do
     {
-    case 1:
-        acceptGoods(stock);
-        break;
-    case 2:
-        shipGoods(stock);
-        break;
-    case 3:
-        info(stock);
-        break;
-    default:
-        return 0;
-        break;
-    }
+        cout << "Веберите, что хотите сделать:" << endl;
+        cout << "1 - принять товар на склад:" << endl;
+        cout << "2 - отгрузить товар со склада:" << endl;
+        cout << "3 - вевести иформацию о товаре на складе:" << endl;
+        cin >> n;
+        cin.get();
+
+        switch (n)
+        {
+        case 1:
+            acceptGoods(stock);
+            break;
+        case 2:
+            shipGoods(stock);
+            break;
+        case 3:
+            info(stock);
+            break;
+        }
+    } while ((n == 1) || (n == 2) || (n == 3));
     return 0;
 }
 
-int isGoodOnStock(vector <product> stock, string name)
+int isGoodOnStock(vector<product>& stock, string name)
 {
-    int cnt=0;
-    for(product elem: stock)
+    int cnt = 0;
+    for (product elem : stock)
     {
-        if (elem.name==name) return cnt;
+        if (elem.name == name)
+            return cnt;
         cnt++;
     }
     return -1;
 }
 
-void acceptGoods(vector <product> stock)
+void acceptGoods(vector<product>& stock)
 {
-    cout<<"Введите название товара:"<<endl;
+    cout << "Введите название товара:" << endl;
     string name;
-    cin>>name;
-    cout<<"Введите количество товара:"<<endl;
+    getline(cin,name);
+    cout << "Введите количество товара:" << endl;
     int addQuantity;
-    cin>>addQuantity;
-    int index=isGoodOnStock(stock, name);
-    if (index>-1)
+    cin >> addQuantity;
+    cin.get();
+    int index = isGoodOnStock(stock, name);
+    if (index > -1)
     {
-        stock[index].quantity+=addQuantity;
+        stock[index].quantity += addQuantity;
     }
     else
     {
-        stock.push_back({name,addQuantity});
+        stock.push_back({ name, addQuantity });
     }
-    
 }
 
-void shipGoods(vector <product>)
-{}
-void info(vector <product> stock)
+void shipGoods(vector<product>& stock)
 {
-    for(product elem: stock)
+    cout << "Введите название товара:" << endl;
+    string name;
+    getline(cin, name);
+    cout << "Введите количество товара:" << endl;
+    int decQuantity;
+    cin >> decQuantity;
+    cin.get();
+    int index = isGoodOnStock(stock, name);
+    if (index > -1)
     {
-        cout<<elem.name<<" "<<elem.quantity<<endl;
+        if (stock[index].quantity >= decQuantity)
+        {
+            stock[index].quantity -= decQuantity;
+        }
+        else
+        {
+            cout << "На складе недостаточно этого товара для отгрузки"<<endl;
+        }
+    }
+    else
+    {
+        cout<<"На складе нет такого товара"<<endl;
+    }
+}
+void info(vector<product>& stock)
+{
+    for (product elem : stock)
+    {
+        cout << elem.name << " " << elem.quantity << endl;
     }
 }
