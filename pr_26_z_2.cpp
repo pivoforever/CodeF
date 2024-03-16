2a7267a5a7ef414227bb2b3029b9e00214bf6b68
Vit <pivonever@gmail.com>
2024-03-16 13:50:06 +0500

    Сделал пз26 зад2

--- pr_26_z_2.cpp
+++ pr_26_z_2.cpp
@@ -15,27 +15,40 @@ int main(int argc, char const *argv[])
     // SetConsoleCP(1251);
     // SetConsoleOutputCP(1251);
     int n;
-    cout << "Введите количество чисел:";
+    cout<<"Введите количество чисел:";
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
-        cout << triBonachy(i) << "\t";
+        cout<<triBonachy(i)<<"\t";
     }
-
+    
     return 0;
 }
 
 int triBonachy(int n)
 {
-    if (n == 1)
+    int x1=0,x2=1,x3=1, tmp;
+    if (n==1)
+    {
         return 0;
-    else if ((n == 2) || (n == 3))
+    }
+    else if ((n==2)||(n==3))
     {
         return 1;
     }
-
     else
     {
-        return triBonachy(n - 3) + triBonachy(n - 2) + triBonachy(n - 1);
+        for (int i = 4; i <= n; i++)
+        {
+            tmp=x1+x2+x3;
+            x1=x2;
+            x2=x3;
+            x3=tmp;
+        }
+        return tmp;
+        
     }
+    
+    
+    
 }
